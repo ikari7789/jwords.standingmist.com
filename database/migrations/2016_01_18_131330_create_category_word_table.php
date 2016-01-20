af6eed22e51cf76db8d66dd86dc3f7f16547ce64
Andrew Miller <ikari7789@yahoo.com>
2016-01-20 03:23:36 +0000

    Fixed table name in migration.

--- database/migrations/2016_01_18_131330_create_category_word_table.php
+++ database/migrations/2016_01_18_131330_create_category_word_table.php
@@ -28,6 +28,6 @@ class CreateCategoryWordTable extends Migration
      */
     public function down()
     {
-        Schema::drop('categories_words');
+        Schema::drop('category_word');
     }
 }
